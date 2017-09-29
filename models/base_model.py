c6523f41f123af6d492c5cd804ae1354426ddc48
wendysegura <118@holbertonschool.com>
2017-09-29 22:16:57 +0000

    working base #3

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-from datetime import datetime, strftime
+from datetime import datetime
+from time import strftime
 import uuid
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 """defines all common attributes/methods for other classes"""
 
 
@@ -9,12 +13,28 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         """assign with the current datetime when an instance is created"""
-        self.created_at = datetime.created_at()
-	"""will be updated every time you change your object"""
-        self.updated_at = datetime.updated_at()
+        self.created_at = datetime.now()
+        """will be updated every time you change your object"""
+        self.updated_at = self.created_at
 
     def __str__(self):
         """print class name, id, dict"""
-        print([<class name>] (<self.id>) <self.__dict__>)
+        '''class_name = self.__class__.__name__
+        s_id = self.id
+        s_dict = self.__dict__'''
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+    def save(self):
+        """updates the public instance attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of"""
+        dictionary_copy = self.__dict__.copy()
+        """add class key and values to the copy"""
+        dictionary_copy["__class__"] = type(self).__name__
+        dictionary_copy["created_at"] = self.created_at.isoformat()
+        dictionary_copy["updated_at"] = self.updated_at.isoformat()
+        return dictionary_copy
+        
 
