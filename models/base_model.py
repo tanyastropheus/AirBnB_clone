095781e876e8a24ae94972fcce958bfdb7a5085c
wendysegura <118@holbertonschool.com>
2017-09-29 01:12:13 +0000

    working on Base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-
+from datetime import datetime, strftime
+import uuid
 """defines all common attributes/methods for other classes"""
 
 
 class BaseModel:
     """Public instance attributes"""
-    id = uuid.uuid4()
-     
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        """assign with the current datetime when an instance is created"""
+        self.created_at = datetime.created_at()
+	"""will be updated every time you change your object"""
+        self.updated_at = datetime.updated_at()
+
+    def __str__(self):
+        """print class name, id, dict"""
+        print([<class name>] (<self.id>) <self.__dict__>)
+
+
