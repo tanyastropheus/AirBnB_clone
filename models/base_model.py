b178bdac9f13f00d5b731e0aefca109450e6519d
wendysegura <118@holbertonschool.com>
2017-09-30 03:49:49 +0000

    working #4

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,28 @@ import uuid
 
 class BaseModel:
     """Public instance attributes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        """assign with the current datetime when an instance is created"""
-        self.created_at = datetime.now()
-        """will be updated every time you change your object"""
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            if 'id' in kwargs:
+                self.id = kwargs["id"]
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            """assign with the current datetime when an instance is created"""
+            self.created_at = datetime.now()
+            """will be updated every time you change your object"""
+            self.updated_at = self.created_at
 
     def __str__(self):
         """print class name, id, dict"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        a = self.__class__.__name__
+        b = self.id
+        c = self.__dict__
+        return "[{}] ({}) {}".format(a, b, c)
 
     def save(self):
         """updates the public instance attribute"""
@@ -31,5 +43,3 @@ class BaseModel:
         dictionary_copy["created_at"] = self.created_at.isoformat()
         dictionary_copy["updated_at"] = self.updated_at.isoformat()
         return dictionary_copy
-        
-
