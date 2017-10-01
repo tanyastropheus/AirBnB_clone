549ee71a41915192b9e4f736591389b9d6a9831b
wendysegura <118@holbertonschool.com>
2017-10-01 23:10:41 +0000

    finished and working #6

--- console.py
+++ console.py
@@ -1,1 +1,35 @@
-"""file is the entry point of our command interpreter"""
+#!/usr/bin/python3
+"""Console"""
+
+
+import cmd
+import models
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    intro = """
+                **************************************************************
+                *                                                            *
+                *                  'Welcome to HBNBC!'                       *
+                *                                                            *
+                *              'Type help to list commands'                  *
+                *                                                            *
+                **************************************************************
+            """
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """quit to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """exit the program"""
+        return True
+
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
