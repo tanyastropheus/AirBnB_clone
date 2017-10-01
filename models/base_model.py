4869d0475c7f12fcf148ea41d10aa02d3711fc0e
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-01 04:47:32 +0000

    task 3 working; need to fix task 4 and test task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from datetime import datetime
+from models import storage # import var created in __init__ under models/
 import uuid
-
 
 """defines all common attributes/methods for other classes"""
 
@@ -9,20 +9,28 @@ import uuid
 class BaseModel:
     """Public instance attributes"""
     def __init__(self, *args, **kwargs):
-        time = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
-            if 'id' in kwargs:
-                self.id = kwargs["id"]
-            if "created_at" in kwargs:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            if "updated_at" in kwargs:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        if kwargs is None:  # if it's a new instance (not from dict reprsntion)
+            storage.new()  # store instance in dictionary to save to file later
+
         else:
-            self.id = str(uuid.uuid4())
-            """assign with the current datetime when an instance is created"""
-            self.created_at = datetime.now()
-            """will be updated every time you change your object"""
-            self.updated_at = self.created_at
+            print("test: ", kwargs.items())
+
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    v = self.to_datetime(v)
+                setattr(self, k, v)
+
+            if 'id' not in self.__dict__:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in self.__dict__:
+                self.created_at = datetime.now()
+            if 'updated_at' not in self.__dict__:
+                self.updated_at = self.created_at
+
+    def to_datetime(self, string):
+        """convert datetime string to datetime object"""
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        return datetime.strptime(string, time)
 
     def __str__(self):
         """print class name, id, dict"""
@@ -34,6 +42,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,41 @@
-"""storage classes """
+#!/usr/bin/python3
+import json
+
+
+class FileStorage:
+    """serializes instances to a JSON file & deserializes back to instances
+
+    Attributes:
+       __file_path (str): path to the JSON file (ec: file.JSON)
+       __objects (dict): empty but will store all objects by <class name>.id
+
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """return the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """set in __objects the obj with key <obj class name>.id"""
+        class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(class_name, obj.id)] = obj.__dict__
+
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        try:
+            with open(FileStorage.__file_path, 'w') as f:  # 'w' vs 'a'?
+                json.dump(FileStorage.__objects, f)
+        except FileNotFoundError:
+            print ("json file does not exist")  # what action should we take?
+
+
+    def reload(self):
+        """deserializes the JSON file to __objects if JSON file exists"""
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
+        except:
+            pass
