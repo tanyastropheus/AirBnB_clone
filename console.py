8e9d42d294c3599810bf79235b2f0f9a62cfd52b
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-03 00:26:10 +0000

    do_show() working

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import models
 from models.engine.file_storage import FileStorage
 
+
 class HBNBCommand(cmd.Cmd):
     intro = """
                 **************************************************************
@@ -29,7 +30,8 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """
-        overloads the emptyline method (that would revert back to the last command);
+        overloads the emptyline method (that would revert back to the last
+        command);
         This will make console display prompt again.
         """
         pass
@@ -40,8 +42,8 @@ class HBNBCommand(cmd.Cmd):
         """Creates a new instance of BaseModel"""
         arg = arg.split()
         if arg[0] and len(arg) == 1:
-             new_instance = BaseModel()
-             print(new_instance.id)
+            new_instance = BaseModel()
+            print(new_instance.id)
 
         elif len(arg) == 0 or arg is None:
             """If the class name is missing"""
@@ -53,9 +55,37 @@ class HBNBCommand(cmd.Cmd):
         models.storage.save()
 
     def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        from models.engine.file_storage import FileStorage
+
+        arg = arg.split()
+        class_list = ['BaseModel']
+        """get FileStorage.__objects"""
+        stored_objects = models.storage.all()
+        id_list = [k.split(".")[1] for k in stored_objects.keys()]
+
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif arg[0] not in class_list:
+            print("** class doesn't exist **")
+
+        elif len(arg) == 1:
+            """missing id"""
+            print("** instance id missing **")
+
+        elif arg[1] not in id_list:
+            """given id does not exist"""
+            print("** instance id missing **")
 
 
-
+        else:
+            """get '<class_name>.id' to match key in FileStorage.__objects"""
+            instance = "{}.{}".format(arg[0], arg[1])
+            """find matching class object"""
+            for k, v in stored_objects.items():
+                if k == instance:
+                    print(v)
 
 
 if __name__ == '__main__':
