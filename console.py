04ca86341020f051437e02ad4b2db482430a1e27
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-03 05:46:01 +0000

    do_destroy() done

--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 import json
 import cmd
 import models
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
@@ -19,6 +20,8 @@ class HBNBCommand(cmd.Cmd):
             """
 
     prompt = "(hbnb) "
+
+    class_list = ['BaseModel']  # need to revisit to make to moduler
 
     def do_quit(self, arg):
         """quit to exit the program"""
@@ -37,29 +40,29 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """import withing method otherwise circular"""
-        from models.base_model import BaseModel
         """Creates a new instance of BaseModel"""
+
         arg = arg.split()
         if arg[0] and len(arg) == 1:
+            # need to check against a list of given class names
             new_instance = BaseModel()
             print(new_instance.id)
 
-        elif len(arg) == 0 or arg is None:
+        elif len(arg) == 0:
             """If the class name is missing"""
             print("** class name missing **")
+
         else:
             """If the class name doesn't exist"""
             print("** class doesn't exist **")
+
         """saves it (to the JSON file) """
         models.storage.save()
 
     def do_show(self, arg):
         """Prints the string representation of an instance"""
-        from models.engine.file_storage import FileStorage
 
         arg = arg.split()
-        class_list = ['BaseModel']
         """get FileStorage.__objects"""
         stored_objects = models.storage.all()
         """create a list of id from storage"""
@@ -68,7 +71,7 @@ class HBNBCommand(cmd.Cmd):
         if len(arg) == 0:
             print("** class name missing **")
 
-        elif arg[0] not in class_list:
+        elif arg[0] not in HBNBCommand.class_list:
             print("** class doesn't exist **")
 
         elif len(arg) == 1:
@@ -77,16 +80,45 @@ class HBNBCommand(cmd.Cmd):
 
         elif arg[1] not in id_list:
             """given id does not exist"""
-            print("** instance id missing **")
+            print("** no instance found **")
 
         else:
-            """get '<class_name>.id' to match key in FileStorage.__objects"""
+            """get '<class_name>.id' to FileStorage.__objects key format"""
             instance = "{}.{}".format(arg[0], arg[1])
-            """find matching class object"""
+            """search matching class object"""
             for k, v in stored_objects.items():
                 if k == instance:
                     print(v)
 
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id & save the change
+        into the JSON file.
+        """
+        arg = arg.split()
+        stored_objects = models.storage.all()
+        id_list = [k.split(".")[1] for k in stored_objects.keys()]
+
+        if len(arg) == 0:
+            '''if class name is missing'''
+            print("** class name missing **")
+
+        elif arg[0] not in HBNBCommand.class_list:
+            print("** class doesn't exist **")
+
+        elif len(arg) == 1:
+            print("** instance id missing **")
+
+        elif arg[1] not in id_list:
+            print("** no instance found **")
+
+        else:
+            instance = "{}.{}".format(arg[0], arg[1])
+            """delete from FileStorage.__objects"""
+            if instance in stored_objects.keys():
+                del stored_objects[instance]
+            """overwrite the new data to file.json"""
+            models.storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
