11a49f5312678e2363669dfd963e2ff0c830089b
Tanya Kryukova <ttkryukova@gmail.com>
2017-10-07 23:10:45 +0000

    added <class name>.all() functionality to the commandline

--- console.py
+++ console.py
@@ -32,12 +32,12 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Creates a new instance of BaseModel"""
 
-        arg = shlex.split(arg)
-        if len(arg) == 0:
+        args = shlex.split(arg)
+        if len(args) == 0:
             print("** class name missing **")
 
-        elif arg[0] in models.classes:
-            new_instance = models.classes[arg[0]]()
+        elif args[0] in models.classes:
+            new_instance = models.classes[args[0]]()
             print(new_instance.id)
             """saves it (to the JSON file) """
             models.storage.save()
@@ -48,25 +48,25 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """Prints the string representation of an instance"""
-        arg = shlex.split(arg)
+        args = shlex.split(arg)
         """get FileStorage.__objects"""
         stored_objects = models.storage.all()
         """create a list of id from storage"""
         id_list = [k.split(".")[1] for k in stored_objects.keys()]
 
-        if len(arg) == 0:
+        if len(args) == 0:
             print("** class name missing **")
 
-        elif arg[0] not in models.classes:
+        elif args[0] not in models.classes:
             print("** class doesn't exist **")
 
-        elif len(arg) == 1:
+        elif len(args) == 1:
             """missing id"""
             print("** instance id missing **")
 
         else:
             """get '<class_name>.id' to FileStorage.__objects key format"""
-            instance = "{}.{}".format(arg[0], arg[1])
+            instance = "{}.{}".format(args[0], args[1])
             if instance not in stored_objects:
                 """given id does not exist"""
                 print("** no instance found **")
@@ -82,23 +82,23 @@ class HBNBCommand(cmd.Cmd):
         Deletes an instance based on the class name and id & save the change
         into the JSON file.
         """
-        arg = shlex.split(arg)
+        args = shlex.split(args)
         stored_objects = models.storage.all()
         id_list = [k.split(".")[1] for k in stored_objects.keys()]
 
-        if len(arg) == 0:
+        if len(args) == 0:
             '''if class name is missing'''
             print("** class name missing **")
 
-        elif arg[0] not in models.classes:
+        elif args[0] not in models.classes:
             print("** class doesn't exist **")
 
-        elif len(arg) == 1:
+        elif len(args) == 1:
             print("** instance id missing **")
 
         else:
             """get '<class_name>.id' to FileStorage.__objects key format"""
-            instance = "{}.{}".format(arg[0], arg[1])
+            instance = "{}.{}".format(args[0], arg[1])
             if instance not in stored_objects:
                 """given id does not exist"""
                 print("** no instance found **")
@@ -114,47 +114,63 @@ class HBNBCommand(cmd.Cmd):
         Print all string representation of all instances
         based on the class name
         '''
-        arg = shlex.split(arg)
+        args = shlex.split(arg)
         stored_objects = models.storage.all()
 
-        if len(arg) == 1:  # if given class name
-            if arg[0] in models.classes:  # if class name exists
+        if len(args) == 1:  # if given class name
+            if args[0] in models.classes:  # if class name exists
                 for k, v in stored_objects.items():
                     '''print out corresponding instances'''
-                    if arg[0] in k:
+                    if args[0] in k:
                         print(v)
             else:
                 print("** class doesn't exist **")
 
-        elif len(arg) == 0:  # if not specifying class name
+        elif len(args) == 0:  # if not specifying class name
             '''print out all instances'''
             for v in stored_objects.values():
                 print(v)
 
     def do_update(self, arg):
         '''update an instance & save the change to the JSON file'''
-        arg = shlex.split(arg)
+        args = shlex.split(arg)
         stored_objects = models.storage.all()
         id_list = [k.split(".")[1] for k in stored_objects.keys()]
 
-        if len(arg) == 0:
+        if len(args) == 0:
             print("** class name missing **")
-        elif arg[0] not in models.classes:
+        elif args[0] not in models.classes:
             print("** class doesn't exist **")
-        elif len(arg) == 1:
+        elif len(args) == 1:
             print("** instance id missing **")
-        elif arg[1] not in id_list:
+        elif args[1] not in id_list:
             print("** no instance found **")
-        elif len(arg) == 2:
+        elif len(args) == 2:
             print("** attribute name missing **")
-        elif len(arg) == 3:
+        elif len(args) == 3:
             print("** value missing **")
         else:
-            instance = "{}.{}".format(arg[0], arg[1])
+            instance = "{}.{}".format(args[0], args[1])
             obj = stored_objects[instance]
             '''convert to the right attribute value type'''
-            setattr(obj, arg[2], arg[3])
+            setattr(obj, args[2], args[3])
             models.storage.save()
+
+    def default(self, arg):
+        args = arg.split('.')
+        if len(args) == 2:
+            if args[0] in models.classes:
+                if args[1] == 'all()':
+                    stored_objects = models.storage.all()
+                    for k, v in stored_objects.items():
+                        if args[0] in k:
+                            print(v)
+                else:
+                    print("**command not found**")
+            else:
+                print("**class doesn't exist**")
+        else:
+            super().default(arg)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
